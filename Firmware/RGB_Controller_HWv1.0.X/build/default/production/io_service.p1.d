bd2537b0a0bbee101cce768d241b0d3e2465feb7
MrChunckuee <mrchunckuee.psr@gmail.com>
2020-08-02 20:08:10 -0500

    RGB Controller harware v1.0

---
+++ Firmware/RGB_Controller_HWv1.0.X/build/default/production/io_service.p1.d
@@ -0,0 +1,6 @@
+build/default/production/io_service.p1:  \
+io_service.c  \
+main.h  \
+mcu.h  \
+io_service.h  \
+isr.h 
---
+++ Firmware/RGB_Controller_HWv1.0.X/build/default/production/main.p1.d
@@ -0,0 +1,7 @@
+build/default/production/main.p1:  \
+main.c  \
+FUSES_Config.h  \
+main.h  \
+mcu.h  \
+io_service.h  \
+isr.h 
